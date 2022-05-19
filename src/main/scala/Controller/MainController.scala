3773a3512ca3c9714dddebe6cfc02eae17b92a69
Mohamed Mamdouh <63814228+MohamedMamdouh18@users.noreply.github.com>
2022-05-19 18:13:37 +0200

    GUI V0.2.5

--- src/main/scala/Controller/MainController.scala
+++ src/main/scala/Controller/MainController.scala
@@ -1,29 +1,42 @@
 package Controller
 
-import scalafx.event.ActionEvent
-import javafx.scene.control.Button
 import scalafxml.core.macros.sfxml
+import scalafx.event.ActionEvent
+
+import javafx.scene.layout.AnchorPane
+import javafx.scene.control.Button
 
 @sfxml
-class MainController(private val but: Button) {
+class MainController( val menuPane : AnchorPane,
+                       val returnButton : Button) {
 
   def XOStart(event: ActionEvent): Unit = {
-    but.setOnMouseClicked((event) => {
-      println("XOStart")
-    })
+    gameMode(true)
     println("XO")
   }
 
   def ChessStart(event: ActionEvent): Unit = {
+    gameMode(true)
     println("Chess")
   }
 
   def Connect4Start(event: ActionEvent): Unit = {
+    gameMode(true)
     println("Connect-4")
   }
 
   def CheckersStart(event: ActionEvent): Unit = {
+    gameMode(true)
     println("Checkers")
+  }
+
+  def returnMenu(event: ActionEvent): Unit = {
+    gameMode(false)
+  }
+
+  def gameMode(boolean: Boolean) : Unit = {
+    menuPane.setVisible(!boolean)
+    returnButton.setVisible(boolean)
   }
 
 }
