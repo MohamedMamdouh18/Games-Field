586a603fabc5507d882b97614613ca215a7af8ae
Youssef Bazina <60438793+Bazina@users.noreply.github.com>
2022-05-20 16:03:12 +0200

    Refactoring

--- src/main/scala/GameEngines/Engines/Connect4Engine.scala
+++ src/main/scala/GameEngines/Engines/Connect4Engine.scala
@@ -3,19 +3,9 @@ package GameEngines.Engines
 import GameEngines.Drawers.Connect4Drawer
 import GameEngines.GameEngine
 import GameEngines.GamesControllers.Connect4Controller
-import javafx.scene.paint.Color
 
 class Connect4Engine extends GameEngine {
   override val gameController = new Connect4Controller
   override val gameDrawer = new Connect4Drawer
   override var gameBoard: Array[Array[String]] = Array.ofDim[String](6, 7)
-
-
-  //    var gameOver=false
-//    while(!gameOver){
-//      gameController.draw()
-//      gameController.checking( , gameController.turn)
-//      gameController.changeTurns()
-//    }
-
 }
