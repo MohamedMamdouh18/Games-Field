7df92e44140d3647d9b0ad19b763da3f028b4c01
Mento79 <bodyomar79@gmail.com>
2022-05-20 13:55:18 +0200

    connect 4 90%

--- src/main/scala/GameEngines/Drawers/Connect4Drawer.scala
+++ src/main/scala/GameEngines/Drawers/Connect4Drawer.scala
@@ -5,13 +5,16 @@ import javafx.scene.Node
 import javafx.scene.layout.{GridPane, StackPane}
 import javafx.scene.paint.Color
 import javafx.scene.shape.Circle
+import scalafx.scene.input.MouseEvent
+import javafx.event.EventHandler
 
 class Connect4Drawer extends Drawer {
   override var gamePane: StackPane = new StackPane()
   override var drag: (Circle) => Unit = _
-
+  var boardControl: Array[Array[Int]]= _
   override def draw(): GridPane = {
     val board = drawBoard(6, 7, Color.rgb(0, 0, 139), Color.rgb(0, 0, 139), showGridLines = false)
+    extendDrawing(board,drag,boardControl)
     board
   }
 
@@ -24,16 +27,65 @@ class Connect4Drawer extends Drawer {
         if (j % 2 == 0 && i % 2 == 0) circle.setFill(Color.rgb(164, 43, 57))
         else if (j % 2 == 0 && i % 2 == 1) circle.setFill(Color.rgb(255, 221, 99))
         else circle.setFill(Color.rgb(49, 46, 43))
+        circle.setOnMouseClicked(e=>
+          Draggable(circle)
+        )
         GridPane.setColumnIndex(circle, j)
         GridPane.setRowIndex(circle, i)
         board.add(circle, j, i, 1, 1)
         GridPane.setHalignment(circle, HPos.CENTER)
         GridPane.setValignment(circle, VPos.CENTER)
+      }
+    }
+  }
+  def extendDrawing(board: GridPane, Draggable: (Circle) => Unit,boardControl :Array[Array[Int]] ): Unit = {
+    board.setAlignment(Pos.CENTER)
 
+    for (i <- 0 until 6) {
+      for (j <- 0 until 7) {
+        val circle = new Circle(35)
+        if (boardControl(i)(j)==1) circle.setFill(Color.rgb(164, 43, 57))
+        else if (boardControl(i)(j)==2) circle.setFill(Color.rgb(255, 221, 99))
+        else {
+          circle.setFill(Color.rgb(49, 46, 43))
+          circle.setOnMouseClicked(e=>
+            Draggable(circle)
+          )
+        }
+        GridPane.setColumnIndex(circle, j)
+        GridPane.setRowIndex(circle, i)
+        board.add(circle, j, i, 1, 1)
+        GridPane.setHalignment(circle, HPos.CENTER)
+        GridPane.setValignment(circle, VPos.CENTER)
       }
     }
   }
 
+  /*
+  def redraw (boardControl: Array[Array[Int]]): Unit =
+  {
+    for (i <- 0 until 6) {
+      for (j <- 0 until 7) {
+        val circle = new Circle(35)
+        if (j % 2 == 0 && i % 2 == 0) circle.setFill(Color.rgb(164, 43, 57))
+        else if (j % 2 == 0 && i % 2 == 1) circle.setFill(Color.rgb(255, 221, 99))
+        else circle.setFill(Color.rgb(49, 46, 43))
+        circle.setOnMouseClicked(e=>
+          Draggable(circle)
+        )
+        GridPane.setColumnIndex(circle, j)
+        GridPane.setRowIndex(circle, i)
+        board.add(circle, j, i, 1, 1)
+        GridPane.setHalignment(circle, HPos.CENTER)
+        GridPane.setValignment(circle, VPos.CENTER)
+      }
+    }
+  }
+  */
+  def handleEvent(e:MouseEvent)={
+    print("marwan pabloooo")
+
+  }
   override def movementDraw(board: GridPane): Unit = {
 
   }
--- src/main/scala/GameEngines/Engines/Connect4Engine.scala
+++ src/main/scala/GameEngines/Engines/Connect4Engine.scala
@@ -3,9 +3,19 @@ package GameEngines.Engines
 import GameEngines.Drawers.Connect4Drawer
 import GameEngines.GameEngine
 import GameEngines.GamesControllers.Connect4Controller
+import javafx.scene.paint.Color
 
 class Connect4Engine extends GameEngine {
   override val gameController = new Connect4Controller
   override val gameDrawer = new Connect4Drawer
+
+  gameDrawer.boardControl=gameController.boardControl
+  gameDrawer.draw()
+  //    var gameOver=false
+//    while(!gameOver){
+//      gameController.draw()
+//      gameController.checking( , gameController.turn)
+//      gameController.changeTurns()
+//    }
 
 }
