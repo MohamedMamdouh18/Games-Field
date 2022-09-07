9d78e15891bb81baaf1bea4aab435ddc152f96eb
Youssef Bazina <60438793+Bazina@users.noreply.github.com>
2022-09-07 19:56:51 +0200

    - AI introduced

--- src/main/scala/Base/ConcreteAI.scala
+++ src/main/scala/Base/ConcreteAI.scala
@@ -9,9 +9,18 @@ class ConcreteAI extends Player {
   override var observer: GameEngine = _
   override var turn: Array[Int] = _
 
-  override def run(board: Array[Array[Piece]], turn: Array[Int], controller: Controller, drawer: Drawer, buts: GridPane): Unit = {
+  override def run(board: Array[Array[Piece]], turn: Array[Int],
+                   controller: Controller, drawer: Drawer, buts: GridPane): Unit = {
 
   }
 
   override def Movement(source: Node): Unit = {}
+
+  def miniMax(board: Array[Array[Piece]], t: Int, depth: Int): State = {
+    new State(0, 0, 0, 0, 0)
+  }
+
+  def estimator(board: Array[Array[Piece]]): Unit = {
+
+  }
 }
