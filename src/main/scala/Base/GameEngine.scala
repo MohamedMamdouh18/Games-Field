5f08790884d6c946090a7eb8b96d68cc2859b040
Youssef Bazina <60438793+Bazina@users.noreply.github.com>
2022-10-04 19:47:22 +0200

    - Game Engine JavaDoc.

--- src/main/scala/Base/GameEngine.scala
+++ src/main/scala/Base/GameEngine.scala
@@ -9,13 +9,31 @@ abstract class GameEngine(players: Array[Player], gameType: String) {
   var turn: Int = 0
   var gameEnded: Boolean = false
 
+  /***
+   * Initialize the game with its GUI and logic.
+   * @param gamePane the stack pane that will hold the board in the GUI.
+   */
   def startGame(gamePane: StackPane): Unit
 
+  /***
+   * Sets the movement on the board if the game doesn't have pieces in the begin.
+   * @param source Node that the event will run on it.
+   */
   def Movement(source: Node): Unit = {}
 
+  /***
+   * Updates the game loop.
+   */
   def update(): Unit
 
+  /***
+   * Executes player move.
+   * @param source Node that will change in the GUI.
+   */
   def setPlayerMove(source: Node = null): Unit = {}
 
+  /***
+   * Runs the AI agent to play in its turn if there is one in the game.
+   */
   def play(): Unit
 }
